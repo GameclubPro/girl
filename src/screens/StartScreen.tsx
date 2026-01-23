f5513d7e0565a3d1fcce439d23470e9d7d9eda7a
sex <yourname@sex.localdomain>
2026-01-23 21:34:23 +0300

    intro screen

--- src/screens/StartScreen.tsx
+++ src/screens/StartScreen.tsx
@@ -22,14 +22,12 @@ export const StartScreen = ({
           loading="eager"
           decoding="async"
         />
-        <div className="title-copy">
-          <p className="subtitle">
-            <span className="subtitle-line">Создаем новую эру</span>
-            <span className="subtitle-line subtitle-line--accent">
-              красоты вместе
-            </span>
-          </p>
-        </div>
+        <p className="subtitle">
+          <span className="subtitle-line">Создаем новую эру</span>
+          <span className="subtitle-line subtitle-line--accent">
+            красоты вместе
+          </span>
+        </p>
       </div>
 
       <h2 className="animate delay-2">Какая роль вам подходит?</h2>
