b9bae9951ad20b04e68dabc8481294602149dd83
sex <yourname@sex.localdomain>
2026-01-24 04:34:27 +0300

    Revert "intro screen"
    This reverts commit 5b3a238705249c87f18e5540d592d451a032fa2c.

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -39,7 +39,6 @@ declare global {
           onClick: (callback: () => void) => void
           offClick: (callback: () => void) => void
         }
-        initData?: string
         initDataUnsafe?: {
           user?: {
             id: number
