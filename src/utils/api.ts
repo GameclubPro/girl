7e740d98002089830b0d7ec6efc49247b6717706
sex <yourname@sex.localdomain>
2026-01-24 04:34:27 +0300

    Revert "intro screen"
    This reverts commit 1f6d39f77260f5b5835df6a167f27c6fb7fcbda9.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,10 +6,7 @@ const getTelegramInitData = () => {
 const buildAuthHeaders = () => {
   const initData = getTelegramInitData()
   if (!initData) return {}
-  return {
-    Authorization: `tma ${initData}`,
-    'X-Telegram-Init-Data': initData,
-  }
+  return { Authorization: `tma ${initData}` }
 }
 
 const shouldAttachAuth = (input: RequestInfo | URL, apiBase?: string) => {
@@ -52,3 +49,4 @@ export const buildAuthQuery = () => {
 }
 
 export const getAuthInitData = () => getTelegramInitData()
+
